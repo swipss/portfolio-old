5bc50e65158c4e6e478105e2596ffbf184f4c762
swipss <vassiljevsten1@gmail.com>
2022-05-15 23:37:58 +0300

    Create typewriter title

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+ document.addEventListener("DOMContentLoaded", function(event) {
+        typeWriter()
+ })
+ 
+ let i = 0
+ let text = "Hey, I'm Sten."
+ let speed = 100
+
+ function typeWriter() {
+        if (i<text.length) {
+               document.getElementById('title').innerHTML += text.charAt(i)
+               i++
+               setTimeout(typeWriter, speed)
+        }
+ }
+ 
+ document.getElementById("work-button").onclick = function () {
+        location.href = "#projects";
+ };
+ 
  document.getElementById("back").onclick = function () {
         location.href = "#header";
  };
+
+
+
+ 
