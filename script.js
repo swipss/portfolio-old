4f4942255b5459e1926105977afe050dc764c667
swipss <vassiljevsten1@gmail.com>
2022-05-16 01:05:07 +0300

    Add section title animations

--- script.js
+++ script.js
@@ -1,18 +1,92 @@
- document.addEventListener("DOMContentLoaded", function(event) {
-        typeWriter()
- })
- 
- let i = 0
- let text = "Hey, I'm Sten."
- let speed = 100
 
- function typeWriter() {
-        if (i<text.length) {
-               document.getElementById('title').innerHTML += text.charAt(i)
+let title = document.getElementById('title')
+let aboutMe = document.getElementById('about-me')
+let projects = document.getElementById('project-title')
+let work = document.getElementById('work-text')
+let contact = document.getElementById('contact-text')
+let i = 0
+let speed = 100
+window.addEventListener('hashchange', function() {
+       let hash = location.hash
+       if (hash === "#about") {
+              i = 0
+              writeAboutMe()
+              console.log('on about')
+       }
+       else if (hash === "#header") {
+              i = 0
+              writeTitle()
+              console.log('on header')
+       }
+       else if (hash === "#projects") {
+              i = 0
+              writeProjects()
+       }
+       else if (hash === "#work") {
+              i = 0
+              writeWork()
+       }
+       else if (hash === "#contact") {
+              i = 0
+              writeContact()
+       }
+})
+
+
+function writeTitle() {
+       let text = "Hey, I'm Sten."
+        if (i<text.length && title.innerHTML != text) {
+               title.innerHTML += text.charAt(i)
                i++
-               setTimeout(typeWriter, speed)
+               setTimeout(writeTitle, speed)
         }
+        
  }
+
+
+
+function writeAboutMe() {
+       let text = "ABOUT ME"
+       if (i<text.length && aboutMe.innerHTML != text) {
+              aboutMe.innerHTML += text.charAt(i)
+              i++
+              setTimeout(writeAboutMe, speed)
+       }
+}
+
+function writeProjects() {
+       let text = "PROJECTS"
+       if (i<text.length && projects.innerHTML != text){
+              projects.innerHTML += text.charAt(i)
+              i++
+              setTimeout(writeProjects, speed)
+       }
+}
+
+function writeWork() {
+       let text = "WORK"
+       if (i<text.length && work.innerHTML != text){
+              work.innerHTML += text.charAt(i)
+              i++
+              setTimeout(writeWork, speed)
+       }
+}
+
+function writeContact() {
+       let text = "LET'S CONNECT."
+       if (i<text.length && contact.innerHTML != text){
+              contact.innerHTML += text.charAt(i)
+              i++
+              setTimeout(writeContact, speed)
+       }
+}
+
+writeTitle()
+
+
+
+
+
  
  document.getElementById("work-button").onclick = function () {
         location.href = "#projects";
@@ -21,7 +95,5 @@
  document.getElementById("back").onclick = function () {
         location.href = "#header";
  };
-
-
 
  
